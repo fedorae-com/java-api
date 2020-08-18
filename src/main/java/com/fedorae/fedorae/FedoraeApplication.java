9896a00d877186984cfa854efcdb8e28e507fa17
froshims <lazarusgrey37@gmail.com>
2020-08-18 02:15:41 -0700

    New Java Api

---
+++ src/main/java/com/fedorae/fedorae/FedoraeApplication.java
@@ -0,0 +1,13 @@
+package com.fedorae.fedorae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedoraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedoraeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedorae/fedorae/FedoraeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedorae.fedorae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedoraeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
