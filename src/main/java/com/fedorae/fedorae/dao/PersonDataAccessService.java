e7f3a09e6afa49a2caff6cbf5cd6b1a12ef4f785
froshims <lazarusgrey37@gmail.com>
2020-08-18 05:19:50 -0700

    Now connected to a postgresql database

--- src/main/java/com/fedorae/fedorae/dao/PersonDataAccessService.java
+++ src/main/java/com/fedorae/fedorae/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.fedorae.fedorae.dao;
 
 import com.fedorae.fedorae.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,10 +10,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository("fakePersonDao")
+@Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
 
-    private static List<Person> DB = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final List<Person> DB = new ArrayList<>();
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public int insertPerson(UUID id, Person person) {
@@ -21,14 +30,27 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return DB;
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return DB.stream()
-                .filter(person -> person.getId().equals(id))
-                .findFirst();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{id},
+                (resultSet, i) -> {
+                    UUID personId = UUID.fromString(resultSet.getString("id"));
+                    String name = resultSet.getString("name");
+                    return new Person(personId, name);
+                });
+        return Optional.ofNullable(person);
     }
 
     @Override
